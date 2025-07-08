553c58bf092364a7a4803a731ff43e786ccea88f
Acies-Nivedhaa <nivedhaa.ravichandren@aciesglobal.com>
2025-07-08 11:36:30 +0530

    Updated

---
+++ cloud_cost_analysis/semantic_model/model/sqls/combined_predictions.sql
@@ -0,0 +1,15 @@
+select 
+    allocation,
+    year,
+    month,
+    actual_azure_cost,
+    predicted_azure_cost,
+    actual_gcp_cost,
+    predicted_gcp_cost,
+    actual_snowflake_cost,
+    predicted_snowflake_cost,
+    actual_total_cost,
+    predicted_total_cost
+from 
+    lakehouse.nivedhaa.cloud_predictions
+    
