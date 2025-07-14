90cb4f27f54c0847999960f22234777f65d2b08f
Acies-Nivedhaa <nivedhaa.ravichandren@aciesglobal.com>
2025-07-14 14:52:19 +0530

    Updated

--- cloud_cost_analysis/semantic_model/model/sqls/azure_billing.sql
+++ cloud_cost_analysis/semantic_model/model/sqls/azure_billing.sql
@@ -1,7 +1,7 @@
 SELECT
     CAST(azure_usage_date AS TIMESTAMP) AS azure_usage_date,
     azure_region,
-    CAST(standardized_allocation AS STRING) AS allocation,
+    CAST(standardized_allocation AS VARCHAR) AS allocation,
     azure_subscription_name,
     azure_resource_groupname,
     azure_cost
